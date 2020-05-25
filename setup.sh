d58112190d332f6447c6848a4718503dd63fe6e8
htr-tech <htr404@protonmail.com>
2020-05-25 16:28:41 +0600

    Opensourced

--- setup.sh
+++ setup.sh
@@ -1,7 +1,49 @@
-# Created By Tahmid Rayat
-# Github : https://github.com/htr-tech
-# Instagram : @tahmid.rayat
+#!/bin/bash
 
-z="
-";oz=' | |';pz='__) ';cBz='ech ';uBz=';97m';ZDz='/HTR';dz='|  |';nz=' |_ ';WDz='chmo';jDz='atio';uz='_  /';DCz='..\e';XCz='mux/';tBz='1m[\';Sz='[1;9';xBz=' Ins';eDz='l';OBz='1m( ';fDz='/Sho';Dz='clea';bz=' |  ';LCz='atin';wz=') | ';YDz='h';sCz='h/'\'' ';MBz='HTR-';Vz='___ ';dDz='rtur';Iz='m\n"';mCz='tps:';aCz='r/bi';Pz='_   ';oBz='.ray';Oz='    ';iDz='p 2';JBz=' Cre';lBz=';96m';KDz=' /da';TDz='tagr';oCz='thub';cz='2m| ';fz='| | ';tCz='> /d';uDz='this';VDz='/Ins';MCz='g En';ZBz=':\e[';qBz='n"';yz='|__|';gCz='echo';lDz='ccee';BDz='//ww';HBz='  \_';qDz='hort';IBz='___|';gz='m  |';pCz='.com';Wz=' _  ';YBz='hub ';Lz='m   ';ZCz='s/us';LBz=' By ';aDz='-TEC';Gz='tf "';pBz='at\e';UBz='[0m\';tz=' / _';Qz=' \e[';xCz='n/HT';SBz='Raya';WBz='91m)';cCz='ortu';rCz='-tec';eCz='n/Sh';NDz='term';VCz='/com';Tz='2m _';bBz='tr-t';Nz='__ _';QDz='/usr';DBz='|\__';ECz='apt ';Kz='1;93';PDz='iles';FCz='upda';HCz='inst';fBz='e[0m';GDz='tahm';YCz='file';gBz='\e[1';kz='  __';XDz='d +x';FDz='com/';uCz='n/ht';yBz='tall';az='   |';Cz=' {';Az='bann';FBz='2m \';Yz='__| ';pDz='92mS';UDz='am';mDz='d !!';MDz='com.';bCz='n/sh';rDz='Url ';dBz='1;97';gDz='rtUr';ICz='all ';iCz='rmux';Jz='m\e[';nBz='hmid';SCz='l /d';TBz='t \e';jCz='-ope';nCz='//gi';CDz='w.in';ODz='ux/f';DDz='stag';WCz='.ter';HDz='id.r';cDz='/sho';GCz='te';aBz='6m h';wCz='ch';PCz='t...';wBz='1m]\';QCz='cp -';Xz='/ __';vz='____';hz=' (__';RCz='f ur';NBz='TECH';QBz='mTah';yCz='R-TE';Mz=' ___';OCz='nmen';EBz='\__|';ADz='CH';XBz=' Git';rBz='}';sBz='er';kCz='n-ur';VBz='e[1;';tDz='Run ';RBz='mid ';EDz='ram.';LDz='ta/d';ACz='ing ';vBz='~\e[';JCz='curl';RDz='/bin';iBz='Inst';ABz=' \ \';Ez='r';iz='_ | ';PBz='1;92';sDz='mto ';TCz='ata/';dCz='rl';kBz='m :\';BBz='| |_';SDz='/ins';GBz='/|_|';Rz='0m\e';lCz='l ht';bDz='H';Zz='|   ';wDz='l \e';KBz='ated';UCz='data';eBz='m| \';vDz=' Too';qCz='/htr';xz='| (_';BCz='Pack';hBz=';93m';Fz='prin';hDz='slee';jBz='agra';ez='__ \';JDz='/'\'' >';NCz='viro';kDz='n Su';qz='\___';Hz='\e[0';fCz='ortU';CCz='ages';mBz=' @ta';CBz='_/|_';lz='_  _';oDz='e \e';Bz='er()';Uz='_ __';vCz='r-te';jz='|__ ';nDz=' Typ';KCz=' -y';sz=' _ \';mz=' __|';rz=' \| ';hCz=' '\''te';IDz='ayat';
-eval "$Az$Bz$Cz$z$Dz$Ez$z$Fz$Gz$Hz$Iz$z$Fz$Gz$Hz$Jz$Kz$Lz$Mz$Nz$Oz$Oz$Oz$Oz$Pz$Qz$Rz$Sz$Tz$Oz$Uz$Vz$Wz$Oz$Hz$Iz$z$Fz$Gz$Hz$Jz$Kz$Lz$Xz$Yz$Zz$Oz$Oz$az$bz$Qz$Rz$Sz$cz$dz$bz$ez$fz$Oz$Hz$Iz$z$Fz$Gz$Hz$Jz$Kz$gz$hz$iz$jz$kz$lz$mz$nz$Qz$Rz$Sz$cz$dz$oz$pz$fz$Oz$Hz$Iz$z$Fz$Gz$Hz$Jz$Kz$Lz$qz$rz$sz$tz$rz$mz$mz$Qz$Rz$Sz$cz$dz$bz$uz$fz$Oz$Hz$Iz$z$Fz$Gz$Hz$Jz$Kz$Lz$vz$wz$fz$xz$wz$dz$nz$Qz$Rz$Sz$cz$yz$oz$ABz$BBz$Vz$Hz$Iz$z$Fz$Gz$Hz$Jz$Kz$gz$vz$CBz$BBz$DBz$CBz$Zz$EBz$Qz$Rz$Sz$FBz$vz$GBz$HBz$qz$IBz$Hz$Iz$z$Fz$Gz$Hz$Iz$z$Fz$Gz$Hz$Jz$Kz$Lz$Oz$JBz$KBz$LBz$MBz$NBz$Qz$Rz$Sz$OBz$Hz$Jz$PBz$QBz$RBz$SBz$TBz$UBz$VBz$WBz$Hz$Iz$z$Fz$Gz$Hz$Iz$z$Fz$Gz$Hz$Jz$Kz$Lz$XBz$YBz$ZBz$Rz$Sz$aBz$bBz$cBz$Hz$Jz$dBz$eBz$fBz$gBz$hBz$iBz$jBz$kBz$fBz$gBz$lBz$mBz$nBz$oBz$pBz$UBz$qBz$z$rBz$z$Az$sBz$z$Fz$Gz$Hz$Iz$z$Fz$Gz$Hz$Iz$z$Fz$Gz$Qz$Rz$Sz$tBz$fBz$gBz$uBz$vBz$Rz$Sz$wBz$fBz$gBz$hBz$xBz$yBz$ACz$BCz$CCz$DCz$UBz$qBz$z$Fz$Gz$Hz$Iz$z$ECz$FCz$GCz$z$ECz$HCz$ICz$JCz$KCz$z$Az$sBz$z$Fz$Gz$Hz$Iz$z$Fz$Gz$Hz$Iz$z$Fz$Gz$Qz$Rz$Sz$tBz$fBz$gBz$uBz$vBz$Rz$Sz$wBz$fBz$gBz$hBz$JBz$LCz$MCz$NCz$OCz$PCz$Hz$Iz$z$Fz$Gz$Hz$Iz$z$QCz$RCz$SCz$TCz$UCz$VCz$WCz$XCz$YCz$ZCz$aCz$bCz$cCz$dCz$z$QCz$RCz$SCz$TCz$UCz$VCz$WCz$XCz$YCz$ZCz$aCz$eCz$fCz$dCz$z$gCz$hCz$iCz$jCz$kCz$lCz$mCz$nCz$oCz$pCz$qCz$rCz$sCz$tCz$TCz$UCz$VCz$WCz$XCz$YCz$ZCz$aCz$uCz$vCz$wCz$z$gCz$hCz$iCz$jCz$kCz$lCz$mCz$nCz$oCz$pCz$qCz$rCz$sCz$tCz$TCz$UCz$VCz$WCz$XCz$YCz$ZCz$aCz$xCz$yCz$ADz$z$gCz$hCz$iCz$jCz$kCz$lCz$mCz$BDz$CDz$DDz$EDz$FDz$GDz$HDz$IDz$JDz$KDz$LDz$TCz$MDz$NDz$ODz$PDz$QDz$RDz$SDz$TDz$UDz$z$gCz$hCz$iCz$jCz$kCz$lCz$mCz$BDz$CDz$DDz$EDz$FDz$GDz$HDz$IDz$JDz$KDz$LDz$TCz$MDz$NDz$ODz$PDz$QDz$RDz$VDz$TDz$UDz$z$WDz$XDz$KDz$LDz$TCz$MDz$NDz$ODz$PDz$QDz$RDz$qCz$rCz$YDz$z$WDz$XDz$KDz$LDz$TCz$MDz$NDz$ODz$PDz$QDz$RDz$ZDz$aDz$bDz$z$WDz$XDz$KDz$LDz$TCz$MDz$NDz$ODz$PDz$QDz$RDz$SDz$TDz$UDz$z$WDz$XDz$KDz$LDz$TCz$MDz$NDz$ODz$PDz$QDz$RDz$VDz$TDz$UDz$z$WDz$XDz$KDz$LDz$TCz$MDz$NDz$ODz$PDz$QDz$RDz$cDz$dDz$eDz$z$WDz$XDz$KDz$LDz$TCz$MDz$NDz$ODz$PDz$QDz$RDz$fDz$gDz$eDz$z$hDz$iDz$z$Fz$Gz$Hz$Iz$z$Fz$Gz$Qz$Rz$Sz$tBz$fBz$gBz$uBz$vBz$Rz$Sz$wBz$fBz$gBz$hBz$xBz$yBz$jDz$kDz$lDz$mDz$Hz$Iz$z$hDz$iDz$z$Fz$Gz$Hz$Iz$z$Fz$Gz$Hz$Iz$z$Fz$Gz$Qz$Rz$Sz$tBz$fBz$gBz$uBz$vBz$Rz$Sz$wBz$fBz$gBz$hBz$nDz$oDz$UBz$VBz$pDz$qDz$rDz$Hz$Jz$Kz$sDz$tDz$uDz$vDz$wDz$UBz$qBz$z$Fz$Gz$Hz$Iz$z$Fz$Gz$Hz$Iz" 
+banner() {
+clear
+printf "\e[0m\n"
+printf "\e[0m\e[1;93m    _____ _                _    \e[0m\e[1;92m _    _ _____  _      \e[0m\n"
+printf "\e[0m\e[1;93m   / ____| |              | |   \e[0m\e[1;92m| |  | |  __ \| |     \e[0m\n"
+printf "\e[0m\e[1;93m  | (___ | |__   ___  _ __| |_  \e[0m\e[1;92m| |  | | |__) | |     \e[0m\n"
+printf "\e[0m\e[1;93m   \___ \|  _ \ / _ \|  __| __| \e[0m\e[1;92m| |  | |  _  /| |     \e[0m\n"
+printf "\e[0m\e[1;93m   ____) | | | | (_) | |  | |_  \e[0m\e[1;92m| |__| | | \ \| |____ \e[0m\n"
+printf "\e[0m\e[1;93m  |_____/|_| |_|\___/|_|   \__| \e[0m\e[1;92m \____/|_|  \_\______|\e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\e[1;93m        Created By HTR-TECH \e[0m\e[1;91m( \e[0m\e[1;92mTahmid Rayat \e[0m\e[1;91m)\e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\e[1;93m    Github :\e[0m\e[1;96m htr-tech \e[0m\e[1;97m| \e[0m\e[1;93mInstagram :\e[0m\e[1;96m @tahmid.rayat\e[0m\n"
+}
+banner
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Installing Packages..\e[0m\n"
+printf "\e[0m\n"
+apt update
+apt install curl -y
+banner
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Creating Environment...\e[0m\n"
+printf "\e[0m\n"
+cp -f url /data/data/com.termux/files/usr/bin/shorturl
+cp -f url /data/data/com.termux/files/usr/bin/ShortUrl
+echo 'termux-open-url https://github.com/htr-tech/' > /data/data/com.termux/files/usr/bin/htr-tech
+echo 'termux-open-url https://github.com/htr-tech/' > /data/data/com.termux/files/usr/bin/HTR-TECH
+echo 'termux-open-url https://www.instagram.com/tahmid.rayat/' > /data/data/com.termux/files/usr/bin/instagram
+echo 'termux-open-url https://www.instagram.com/tahmid.rayat/' > /data/data/com.termux/files/usr/bin/Instagram
+chmod +x /data/data/com.termux/files/usr/bin/htr-tech
+chmod +x /data/data/com.termux/files/usr/bin/HTR-TECH
+chmod +x /data/data/com.termux/files/usr/bin/instagram
+chmod +x /data/data/com.termux/files/usr/bin/Instagram
+chmod +x /data/data/com.termux/files/usr/bin/shorturl
+chmod +x /data/data/com.termux/files/usr/bin/ShortUrl
+sleep 2
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Installation Succeed !!\e[0m\n"
+sleep 2
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Type \e[0m\e[1;92mShortUrl \e[0m\e[1;93mto Run this Tool \e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\n"
